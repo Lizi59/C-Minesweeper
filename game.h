46a1f65163a34e3b01e22500b76e154e21ebf03b
Lizi59 <113404336+Lizi59@users.noreply.github.com>
2023-01-05 18:06:51 +0800

    头文件：函数的声明与某些设置

---
+++ game.h
@@ -0,0 +1,16 @@
+#pragma once
+#define ROW 9
+#define COL 9
+
+#define EASY_COUNT 10
+
+#define ROWS ROW+2
+#define COLS COL+2
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void InitBoard(char board[ROWS][COLS], int rows, int cols,char set);
+void DisplayBoard(char board[ROWS][COLS], int row, int col);
+void setMine(char board[ROWS][COLS], int row, int col);
+void FindMine(char mine[ROWS][COLS],char show[ROWS][COLS],int row,int col);
